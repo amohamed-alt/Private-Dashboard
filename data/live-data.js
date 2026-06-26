8e3b353d7346ac95dd2895b282adde1143b7d4a3
amohamed-alt <a.mohamed@bayt.net>
2026-06-27 01:00:56 +0300

    Update dashboard data - 2026-06-26T22:00:56.152Z

--- data/live-data.js
+++ data/live-data.js
@@ -1,5 +1,5 @@
 window.DASHBOARD_DATA = {
-  "generatedAt": "2026-06-26T16:00:44.647Z",
+  "generatedAt": "2026-06-26T22:00:56.143Z",
   "source": {
     "summaryRows": 77,
     "retentionRows": 188,
@@ -9685,4 +9685,4 @@ window.DASHBOARD_DATA = {
     }
   ]
 };
-window.DASHBOARD_DATA_LOADED_AT = "2026-06-26T16:00:44.650Z";
+window.DASHBOARD_DATA_LOADED_AT = "2026-06-26T22:00:56.152Z";
